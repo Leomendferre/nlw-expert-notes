bda033c6d0f5af8c318b7e79eb1656d3e4903a18
Leomendferre <leo.mend@hotmail.com>
2024-04-21 16:12:40 -0300

    Subindo projeto

---
+++ src/app.tsx
@@ -0,0 +1,81 @@
+import { ChangeEvent, useState } from 'react'
+import logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
+interface Note {
+  id: string
+  date: Date
+  content: string
+}
+
+export function App() {
+  const [search, setSearch] = useState ('')
+
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage)
+    }
+    return []
+
+  })
+
+  //randomUUID Gera um ID em formato de string mas sempre unico.
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(), 
+      date: new Date(),
+      content,
+    }
+
+    const notesArray = [newNote, ...notes]
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filteredNotes = search !== ''
+    ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+    : notes
+
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">{/*Space coloca um espaçamento em todos os elementos*/}
+      <img src={logo} alt="NLW Expert" />
+
+      <form className="w-full">
+       <input 
+          type="text"  
+          placeholder='Busque em suas notas...' 
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          onChange={handleSearch}
+        />
+      </form>
+      
+
+      {/*H-PX adiciona uma linha de separação*/}
+      <div className="h-px bg-slate-700"></div>  
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+
+       {/* <NoteCard note={{
+            date: new Date(),
+            content: 'Hello World'
+          }} /> Criar o componente quando ele se repete diversas vezes ou para deixar algo menor, mais facil de dar manutenção*/} 
+
+        {filteredNotes.map (note =>{
+           return <NoteCard key={note.id} note= {note} />
+        })}
+      </div>
+    </div>
+  )
+}
