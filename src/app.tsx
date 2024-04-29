25b1a4149921759936624c028df1dffc2c01269b
Leomendferre <leo.mend@hotmail.com>
2024-04-29 16:14:20 -0300

    Deixando mais responsivo a aplicação

--- src/app.tsx
+++ src/app.tsx
@@ -48,7 +48,7 @@ export function App() {
     : notes
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">{/*Space coloca um espaçamento em todos os elementos*/}
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">{/*Space coloca um espaçamento em todos os elementos*/}
       <img src={logo} alt="NLW Expert" />
 
       <form className="w-full">
@@ -64,7 +64,7 @@ export function App() {
       {/*H-PX adiciona uma linha de separação*/}
       <div className="h-px bg-slate-700"></div>  
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
        {/* <NoteCard note={{
